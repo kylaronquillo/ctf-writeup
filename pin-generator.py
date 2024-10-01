c26f09d0c545164988f56ff9f08103670947d421
Kyla Ronquillo <109019058+kylaronquillo@users.noreply.github.com>
2024-10-01 21:48:28 +0800

    Delete pin-generator.py

--- pin-generator.py
+++
@@ -1,7 +0,0 @@
-from itertools import product
-
-# Generate PINs with lengths 4 to 6
-with open('pin_wordlist.txt', 'w') as f:
-    for length in range(4, 7):
-        for pin in product('0123456789', repeat=length):
-            f.write(''.join(pin) + '\n')
